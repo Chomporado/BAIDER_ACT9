424d45ade97a219bbb1a25e21e3303d2df958cd9
Jan Fritz Baider <fritz.baider@gmail.com>
2024-06-28 23:56:28 +0800

    ACTIVITY 9

---
+++ app/src/main/java/com/example/marsphotos/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.marsphotos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.marsphotos.ui.MarsPhotosApp
+import com.example.marsphotos.ui.theme.MarsPhotosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            MarsPhotosTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    MarsPhotosApp()
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNow() {
+    MarsPhotosTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            MarsPhotosApp()
+        }
+    }
+}
+
+
+
+
